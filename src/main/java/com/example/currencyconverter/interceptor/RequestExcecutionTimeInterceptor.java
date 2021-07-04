6d7da185127be06520c4a85564c799ab8fa99567
Samyak Jain <jainsamyak330@gmail.com>
2021-07-04 16:30:53 +0530

    Task: Add Server-Timing to request headers

---
+++ src/main/java/com/example/currencyconverter/interceptor/RequestExcecutionTimeInterceptor.java
@@ -0,0 +1,15 @@
+package com.example.currencyconverter.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestExcecutionTimeInterceptor extends HandlerInterceptorAdapter {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
+}
